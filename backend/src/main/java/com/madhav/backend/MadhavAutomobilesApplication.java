76d8a3221e67b9eda8fba8ff4fbb3bd4cc287c5b
soulsToJson <official.agrim02@outlook.com>
2025-10-17 00:57:14 +0530

    Initial project setup with spring boot, Angular, Docker, Mysql

---
+++ backend/src/main/java/com/madhav/backend/MadhavAutomobilesApplication.java
@@ -0,0 +1,13 @@
+package com.madhav.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadhavAutomobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadhavAutomobilesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/madhav/backend/MadhavAutomobilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madhav.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadhavAutomobilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
